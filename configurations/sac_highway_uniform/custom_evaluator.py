86d794add808b866e5de907251b8e8a8e24a2973
Chenyang <tcy1079006289@gmail.com>
2019-10-28 20:28:59 +0100

    add lane-change in sac_highway_uniform

--- configurations/sac_highway_uniform/custom_evaluator.py
+++ configurations/sac_highway_uniform/custom_evaluator.py
@@ -6,11 +6,14 @@ from modules.runtime.commons.parameters import ParameterServer
 from bark.geometry import *
 
 from src.evaluators.goal_reached import GoalReached
+success = 0
 
 class CustomEvaluator(GoalReached):
   """Shows the capability of custom elements inside
      a configuration.
   """
+  #global success
+  #print("success1 = {}".format(str(success//10)))
   def __init__(self,
                params=ParameterServer(),
                eval_agent=None):
@@ -37,8 +40,14 @@ class CustomEvaluator(GoalReached):
     agent = world.agents[self._eval_agent]
     agent_state = agent.state
     centerline = agent.local_map.get_driving_corridor().center
-    # TODO(@hart): HACK; to see whether a lane-change can be learned
-    agent_xy = Point2d(agent.state[1] + 4., agent.state[2])
+    
+
+    lane_change = \
+    self._params["ML"]["Maneuver"]["lane_change"]
+    if lane_change == 1:
+      agent_xy = Point2d(agent.state[1] + 4., agent.state[2])
+    else:
+      agent_xy = Point2d(agent.state[1], agent.state[2])
     return distance(centerline, agent_xy)
 
   def _evaluate(self, world, eval_results):
@@ -48,16 +57,27 @@ class CustomEvaluator(GoalReached):
     # print(self._params["ML"]["Maneuver"]["lane_change"])
     agent_state = world.agents[self._eval_agent].state
     done = False
-    success = eval_results["goal_reached"]
+    global success
+
+    success += eval_results["goal_reached"]
+    # print("success1 = {}".format(str(success)))
+    self._params["ML"]["Maneuver"]["success"] = success
     distance = self._distance_to_center_line(world)
     collision = eval_results["ego_collision"]
     step_count = eval_results["step_count"]
     # determine whether the simulation should terminate
-    if success or collision or step_count > self._max_steps:
+    if (success//5) or collision or step_count > self._max_steps:
       done = True
     # calculate reward
+    # print("success1 = {}".format(str(success)))
+    # print("goal = {}".format(str(self._goal_reward)))
+    # print("distance = {}".format(str(distance)))
+    # print("collision = {}".format(str(collision)))
     reward = collision * self._collision_penalty + \
       success * self._goal_reward - 0.1*distance
+    # print("reward = {}".format(str(reward)))
+    if done == True:
+      success = 0
     return reward, done, eval_results
     
 
---
+++ configurations/sac_highway_uniform/custom_observer.py
@@ -0,0 +1,33 @@
+import numpy as np
+from bark.models.dynamic import StateDefinition
+from modules.runtime.commons.parameters import ParameterServer
+import math
+import operator
+from src.commons.spaces import BoundedContinuous, Discrete
+
+from src.observers.simple_observer import SimpleObserver
+
+class CustomObserver(SimpleObserver):
+  def __init__(self, params=ParameterServer()):
+    SimpleObserver.__init__(self,
+                            params)
+    self._perform_lane_change = False
+    self._observation_len = \
+      self._max_num_vehicles*self._len_state + 1
+
+  def observe(self, world, agents_to_observe):
+    extended_state = super(CustomObserver, self).observe(world, agents_to_observe)
+    extended_state[-1] = self._params["ML"]["Maneuver"]["lane_change"]
+    # extended_state[-1] = self._params["ML"]["Maneuver"]["slow_down"]
+    return extended_state
+
+  def reset(self, world, agents_to_observe):
+    super(CustomObserver, self).reset(world, agents_to_observe)
+    rn = np.random.randint(0, 2)
+    # rn = 0
+    # sd = np.random.randint(0, 2)
+    self._params["ML"]["Maneuver"]["lane_change",
+      "Whether a lane change should be performed or not.",
+       rn] = rn
+    # self._params["ML"]["Maneuver"]["slow_down"] = sd
+    return world
